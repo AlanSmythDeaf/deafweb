3bc96bead0a64c5bbb24e76832b5cd3a084003af
Alan Smyth <alan_smyth@hotmail.com>
2025-03-29 13:34:02 +0000

    Update subscribe button css and header

--- contact/admin.py
+++ contact/admin.py
@@ -8,3 +8,11 @@ from django_summernote.admin import SummernoteModelAdmin
 class ContactFormAdmin(SummernoteModelAdmin):
     list_display = ("name", "phonenumber", "email", "message")
     search_fields = ["name", "email"]
+
+    def get_readonly_fields(self, request, obj=None):
+        """
+        Make all fields read-only in the admin interface.
+        """
+        if obj:  # When editing an existing object
+            return [field.name for field in self.model._meta.fields]
+        return self.readonly_fields
--- deafdesign/settings.py
+++ deafdesign/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-alansmythdeaf-deafweb-x0o5rl5dx2u.ws-eu118.gitpod.io',
                  'ddesign-pp5-01f27d10398c.herokuapp.com',
