158ddf747c52d6bf9e0b736da43d2403390a7903
Alan Smyth <alan_smyth@hotmail.com>
2025-06-23 09:05:13 +0000

    Add faq as a crud in about page and add manage edit html page

--- about/urls.py
+++ about/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from about.views import about
+from .views import about, manage_faq, delete_faq, edit_faq
 
 urlpatterns = [
     path('', about, name='about'),
+    path('manage-faq/', manage_faq, name='manage_faq'),
+    path('delete-faq/<int:faq_id>/', delete_faq, name='delete_faq'),
+    path('edit-faq/<int:faq_id>/', edit_faq, name='edit_faq'),
 ]
+
--- about/views.py
+++ about/views.py
@@ -1,7 +1,42 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.admin.views.decorators import staff_member_required
+from .models import FAQ
+from .forms import FAQForm
 
 def about(request):
-    return render(request, 'about/about.html')
+    faqs = FAQ.objects.all()
+    is_admin = request.user.is_authenticated and request.user.is_staff
+    context = {
+        'faqs': faqs,
+        'is_admin': is_admin,
+    }
+    return render(request, 'about/about.html', context)
+
+@staff_member_required
+def manage_faq(request):
+    faqs = FAQ.objects.all()
+    if request.method == 'POST':
+        form = FAQForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('about')
+    else:
+        form = FAQForm()
+    return render(request, 'about/manage_faq.html', {'form': form, 'faqs': faqs})
+
+@staff_member_required
+def delete_faq(request, faq_id):
+    FAQ.objects.filter(id=faq_id).delete()
+    return redirect('manage_faq')
+
+@staff_member_required
+def edit_faq(request, faq_id):
+    faq = get_object_or_404(FAQ, id=faq_id)
+    if request.method == 'POST':
+        form = FAQForm(request.POST, instance=faq)
+        if form.is_valid():
+            form.save()
+            return redirect('manage_faq')
+    else:
+        form = FAQForm(instance=faq)
+    return render(request, 'about/edit_faq.html', {'form': form, 'faq': faq})
